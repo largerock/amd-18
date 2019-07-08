551acb66cf92dc96dd0d016d17f27407dcea6d6d
largerock <largerock1@gmail.com>
2019-07-08 19:59:15 +1200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -P ~/Downloads/ "https://drivers.amd.com/drivers/linux/amdgpu-pro-19.20-812932-ubuntu-18.04.tar.xz"
+tar -Jxvf amdgpu-pro-19.20-812932-ubuntu-18.04.tar.xz
+
+cd ~/Downloads/amdgpu-pro-19.20-812932-ubuntu-18.04
+
