bb832f1f7308af15f6476681880e7a1ee0cdea51
largerock <largerock1@gmail.com>
2019-07-08 20:04:36 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ wget -P ~/Downloads/ "https://drivers.amd.com/drivers/linux/amdgpu-pro-19.20-812
 cd ~/Downloads
 tar -Jxvf amdgpu-pro-19.20-812932-ubuntu-18.04.tar.xz
 cd ~/Downloads/amdgpu-pro-19.20-812932-ubuntu-18.04
-./amdgpu-install --opencl=pal,legacy
+./amdgpu-pro-install -y --opencl=pal,legacy --headless
+echo "you better reboot boi"
