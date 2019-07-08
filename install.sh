d353cff4ac2c9e95852ddc4b2481de5b6b3e0e36
largerock <largerock1@gmail.com>
2019-07-09 09:10:40 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~
+cd ~/
 export fileid=1oUq953eyUkg2bOQJSO7cx3c-pwd3wZfc
 export filename=amdgpu-pro.tar.xz
 
@@ -10,6 +10,6 @@ wget --load-cookies cookies.txt -O $filename \
      'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
 
 tar -Jxvf amdgpu-pro.tar.xz
-cd ~/amdgpu-pro
+cd ~/amdgpu-pro/
 ./amdgpu-install -y --opencl=pal,legacy
 echo "you better reboot boi"
